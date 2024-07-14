14f45efd2aac27ad87ea09c43a427dea613104d9
fordovodec2 <fordovodec@gmail.com>
2024-07-14 19:31:46 +0700

    Добавил ожидание

--- test/test_3_create_default_out_scenario.py
+++ test/test_3_create_default_out_scenario.py
@@ -7,6 +7,7 @@ def test_create_out_scenario(page: Page, context: BrowserContext):
     out = OutboundScenarios(page)
     out.open_page_outbound()
     out.open_outbound_scenarios()
+    out.waiting_10_seconds()
     out.click_add_scenario()
     out.rename_out_scenario_in_editor()
     out.click_publish_in_editor()
